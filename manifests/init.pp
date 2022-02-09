7f33178ec2a319e405811b0ac3530958906accfa
Ben Sperry <bsper2@illinois.edu>
2022-02-09 10:44:28 -0600

    Start setting up sssd template

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,13 @@
 # @summary Install and configure tlog
 #
+# @parm enable_session_log
+#   Enables or disabled the logging of sessions
+#
 # @example
 #   include profile_session_log
-class profile_session_log {
+class profile_session_log (
+  Boolean $enable_session_log,
+) {
   include ::profile_session_log::install
   include ::profile_session_log::sssd
 }
--- manifests/sssd.pp
+++ manifests/sssd.pp
@@ -3,4 +3,23 @@
 # @example
 #   include profile_session_log::sssd
 class profile_session_log::sssd {
+
+  $enabled = lookup('profile_session_log::enable_session_log', Boolean)
+
+  if ($enabled) {
+    $ensure_parm = 'present'
+  } else {
+    $ensure_parm = 'absent'
+  }
+
+
+  file { '/etc/sssd/conf.d/sssd-session-recording.conf':
+    ensure  => $ensure_parm,
+    content => epp( 'profile_session_log/sssd-session-recording.conf.epp' ),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    #require => Package['unbound'], #TODO make this require, can it take an array of packages?
+  }
+
 }
